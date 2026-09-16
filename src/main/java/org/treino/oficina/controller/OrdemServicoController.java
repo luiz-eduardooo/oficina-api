1bd9ec43fb15392eea66f7ccab58a2c7a6070aac
luiz-eduardooo <luizeduardo2013051@gmail.com>
2026-09-16 08:34:20 -0300

    Feat: adição do controller de ordem de serviço e swagger.

---
+++ src/main/java/org/treino/oficina/controller/OrdemServicoController.java
@@ -0,0 +1,40 @@
+package org.treino.oficina.controller;
+
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.treino.oficina.dtos.itens.ItemPecaRequestDTO;
+import org.treino.oficina.dtos.itens.ItemServicoRequestDTO;
+import org.treino.oficina.dtos.ordemServico.OrdemServicoResponseDTO;
+import org.treino.oficina.services.OrdemServicoService;
+
+@RestController
+@RequestMapping("/os")
+@RequiredArgsConstructor
+public class OrdemServicoController {
+
+    private final OrdemServicoService ordemServicoService;
+
+    @PostMapping("/{idVeiculo}")
+    public ResponseEntity<OrdemServicoResponseDTO> criarOs(@PathVariable Long idVeiculo){
+        return ResponseEntity.status(201).body(ordemServicoService.criarOs(idVeiculo));
+    }
+
+    @PatchMapping("/{idOs}")
+    public ResponseEntity<OrdemServicoResponseDTO> fecharOs(@PathVariable Long idOs) {
+        return ResponseEntity.ok(ordemServicoService.fecharOs(idOs));
+    }
+
+    @PostMapping("/{idOs}/peca")
+    public ResponseEntity<OrdemServicoResponseDTO> adicionarItemPeca(@PathVariable Long idOs, @Valid @RequestBody ItemPecaRequestDTO dto){
+        return ResponseEntity.status(201).body(ordemServicoService.adicionarItemPeca(idOs, dto));
+    }
+    @PostMapping("/{idOs}/servico")
+    public ResponseEntity<OrdemServicoResponseDTO> adicionarItemServico(@PathVariable Long idOs, @Valid @RequestBody ItemServicoRequestDTO dto){
+        return ResponseEntity.status(201).body(ordemServicoService.adicionarItemServico(idOs, dto));
+    }
+}
+
+
